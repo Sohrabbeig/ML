1f42a95ad23b3a79124002fb1fb0785655f0f9e4
amirhossein <Ahsohrabbeig@gmail.com>
2019-05-23 20:51:28 +0430

    best split

--- HW4/src/decision_tree.py
+++ HW4/src/decision_tree.py
@@ -1,1 +1,35 @@
-from .impurity_measures import entropy, gini_index, miss_classification
+def split(index, value, dataset):
+    left, right = list(), list()
+    for item in dataset:
+        if item[index] < value:
+            left.append(item)
+        else:
+            right.append(item)
+    return left, right
+
+
+def information_gain(impurity_measure, dataset, index, item):
+    class_values = list(set(item[-1] for item in dataset))
+    left, right = split(index, item[index], dataset)
+    left_count = len(left)
+    right_count = len(right)
+    total_count = left_count + right_count
+    if left_count == 0 or right_count == 0:
+        return 0
+    return (impurity_measure(dataset, class_values) \
+           - (left_count / total_count) * impurity_measure(left, class_values) \
+           - (right_count / total_count) * impurity_measure(right, class_values))
+
+
+def get_split(dataset, impurity_measure):
+    b_index, b_value, b_score, b_groups = float('-inf'), float('-inf'), float('-inf'), None
+    for index in range(len(dataset[0]) - 1):
+        i = 1
+        for item in dataset:
+            ig = information_gain(impurity_measure, dataset, index, item)
+            print("index: {}, item: {}, ig: {}".format(index, i, ig))
+            i += 1
+            if ig > b_score:
+                b_index, b_value, b_score, b_groups = index, item[index], ig, split(index, item[index], dataset)
+            print(b_score)
+    return {'index': b_index, 'value': b_value, 'groups': b_groups}
--- HW4/src/impurity_measures.py
+++ HW4/src/impurity_measures.py
@@ -1,55 +1,54 @@
 from math import log2
 
 
-def class_probabilities(data, classes):
+def class_probabilities(dataset, class_values):
     class_count = {}
     class_probability = {}
-
-    for c in classes:
+    for c in class_values:
         class_count[c] = 0
 
-    for item in data:
+    for item in dataset:
         class_count[item[-1]] += 1
 
     for c, n in class_count.items():
-        class_probability[c] = n / len(data)
+        class_probability[c] = n / len(dataset)
 
     return class_probability.items()
 
 
-def gini_index(data, classes):
-    return 1 - sum(p[1] ** 2 for p in class_probabilities(data, classes))
+def gini_index(dataset, class_values):
+    return 1 - sum(p[1] ** 2 for p in class_probabilities(dataset, class_values))
 
 
-def entropy(data, classes):
+def entropy(dataset, class_values):
     try:
-        return -sum(p[1] * log2(p[1]) for p in class_probabilities(data, classes))
+        return -sum(p[1] * log2(p[1]) for p in class_probabilities(dataset, class_values))
     except:
         return 0.0
 
 
-def miss_classification(data, classes):
-    return 1 - max(class_probabilities(data, classes), key=lambda item: item[1])[1]
+def miss_classification(dataset, class_values):
+    return 1 - max(class_probabilities(dataset, class_values), key=lambda item: item[1])[1]
 
-#---------------testing impurity measures---------------
+# ---------------testing impurity measures---------------
 # split1 = [[1, 2, 3, 1],
 #           [1, 3, 2, 1],
 #           [1, 3, 2, 1],
 #           [2, 3, 1, 0],
 #           [2, 3, 1, 0],
 #           [2, 1, 3, 0]]
-#
+# 
 # split2 = [[1, 2, 3, 1],
 #           [1, 3, 2, 1],
 #           [2, 3, 1, 1],
 #           [2, 1, 3, 1]]
+# 
+# class_values = [0, 1]
 #
-# classes = {0, 1}
-#
-# print(miss_classification(split1, classes))
-# print(entropy(split1, classes))
-# print(gini_index(split1, classes))
-#
-# print(miss_classification(split2, classes))
-# print(entropy(split2, classes))
-# print(gini_index(split2, classes))
+# print(miss_classification(split1, class_values))
+# print(entropy(split1, class_values))
+# print(gini_index(split1, class_values))
+# 
+# print(miss_classification(split2, class_values))
+# print(entropy(split2, class_values))
+# print(gini_index(split2, class_values))
